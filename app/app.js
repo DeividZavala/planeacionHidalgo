b7b7515d97f79b4abef9c8dd348dd2cb38f767d8
Héctor Feliciano Campos Muñoz <BlisS@MacBook-Pro-de-Hector.local>
2016-10-04 23:23:41 -0500

    login completado

--- app/app.js
+++ app/app.js
@@ -1,10 +1,35 @@
 (function () {
     angular
         .module('palneacionHidalgo',['firebase','ngRoute'])
-        .controller('logout',logout);
+        .controller('indexCtrl',indexCtrl);
 
-    function logout() {
-        var logout = this;
+    function indexCtrl() {
+        var self = this;
+        self.estado = null;
+
+        this.logOut = function(){
+            firebase.auth().signOut().then(function() {
+               location.reload();
+            }, function(error) {
+              // An error happened.
+            });
+        };
+
+        this.checkLogin = function(){
+            firebase.auth().onAuthStateChanged(function(user) {
+              if (user) {
+                self.estado = true;
+                self.email = user.email;
+                console.log(user);
+              } else {
+                self.estado = false;
+              }
+            });
+            console.log(self.estado);
+            return self.estado;
+        };
     }
+
+    
     
 })();
